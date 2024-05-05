67bad2d2f3cd67fff4b1092b21e04741aa80c405
Svetlana Sycheva <svetlanasych@gmail.com>
2024-05-05 19:33:52 +0200

    Bot with button and unfinished parser

--- get_data_for_analytics_bot/app/handlers.py
+++ get_data_for_analytics_bot/app/handlers.py
@@ -1,18 +1,22 @@
+import os
 from aiogram import Router, types
 from aiogram.filters import CommandStart
 from aiogram.types import Message
 from datetime import datetime
+from dotenv import load_dotenv
 
 import app.keyboards as kb
 from app.keyboards import command
 from app.parser import parser
 
 router = Router()
+load_dotenv()
+user1 = os.getenv('USER1')
 
 
 @router.message(CommandStart())
 async def cmd_start(message: Message):
-    authorized_users = [4123367722, 5876825918, 1598142492]
+    authorized_users = [4178867722, 5877825258, user1]
     if message.from_user.id in authorized_users:
         await message.answer('Hello', reply_markup=kb.button)
     else:
