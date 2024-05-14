6499a803d66389cea7a69c15f0a1f384a4147799
Svetlana Sycheva <svetlanasych@gmail.com>
2024-05-14 16:49:33 +0200

    Async google

--- get_data_for_analytics_bot/app/handlers.py
+++ get_data_for_analytics_bot/app/handlers.py
@@ -1,13 +1,13 @@
 import os
-from aiogram import Router, types
+import asyncio
+from aiogoogle import Aiogoogle
+from aiogram import F, Router, types
 from aiogram.filters import CommandStart
 from aiogram.types import Message
-from datetime import datetime
 from dotenv import load_dotenv
 
 import app.keyboards as kb
-from app.keyboards import command
-from app.parser import parser
+from app.google_table import collect_analytics
 
 router = Router()
 load_dotenv()
@@ -19,16 +19,16 @@ authorized_users = [4178867722, 5877825258, 1598142492, user1]
 async def cmd_start(message: Message):
 
     if message.from_user.id in authorized_users:
-        await message.answer('Hello', reply_markup=kb.button)
+        await message.answer('Нажмите кнопку, чтобы бот собрал аналитику',
+                             reply_markup=kb.button)
     else:
         await message.answer('К сожалению, у вас нет прав доступа')
 
 
-#@router.callback_query_handler(command.filter(function='parse'))
-#async def parser_callback_handler(callback_query: types.CallbackQuery):
-#    await callback_query.answer()
-#    try:
-#        parser.run()
-#    except KeyboardInterrupt:
-#        finish = datetime.today().strftime('%m/%d/%y %H:%M')
-#        print(f'App finished at{finish}')
+@router.callback_query(F.data == 'collect_analytics')
+async def collect_analytics_bot(callback: types.CallbackQuery):
+    await callback.message.answer(await collect_analytics())
+    await callback.answer(
+        text="Спасибо, что воспользовались ботом!",
+        show_alert=True
+    )
