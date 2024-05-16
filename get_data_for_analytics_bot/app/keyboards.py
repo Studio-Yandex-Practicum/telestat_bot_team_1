a2ad19ef6a279a156869d4ab3fc353bb3390ceb9
Davletova Lyudmila <108341911+luydmila-davletova@users.noreply.github.com>
2024-05-17 02:41:29 +0300

    update requirements

--- get_data_for_analytics_bot/app/keyboards.py
+++
@@ -1,8 +0,0 @@
-from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
-
-
-button = InlineKeyboardMarkup(inline_keyboard=[
-    [InlineKeyboardButton(
-        text='начать сбор аналитики',
-        callback_data='collect_analytics')]
-])
