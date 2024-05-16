0440459ed0ac6f7b0a52a668807d151550d5ece9
Сергей Постнов <toosergey@yandex.ru>
2024-05-16 16:57:58 +0300

    channels and choice

--- get_data_for_analytics_bot/database/models.py
+++ get_data_for_analytics_bot/database/models.py
@@ -1,5 +1,6 @@
 from sqlalchemy import DateTime, String, func, Boolean
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from typing_extensions import Optional
 
 
 class Base(DeclarativeBase):
@@ -18,4 +19,13 @@ class UserTG(Base):
 class ChannelTG(Base):
     __tablename__ = 'channel'
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
-    tg_channel_name: Mapped[str] = mapped_column(String(64), nullable=False)
+    channel_id: Mapped[Optional[int]]
+    channel_name: Mapped[str] = mapped_column(String(64), nullable=False)
+    channel_current: Mapped[bool] = mapped_column(Boolean, default=False)
+
+
+class ParsingPROD(Base):
+    __tablename__ = 'period'
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    period_id: Mapped[Optional[int]]
+    period: Mapped[Optional[int]]
--- get_data_for_analytics_bot/filters/chat_types.py
+++ get_data_for_analytics_bot/filters/chat_types.py
@@ -1,13 +1,22 @@
 from aiogram import Bot, types
 from aiogram.filters import Filter
+from aiogram.types import Message
+from typing_extensions import Union
+from typing import List
 
 
 class ChatTypeFilter(Filter):
-    def __init__(self, chat_types: list[str]) -> None:
-        self.chat_types = chat_types
+    def __init__(self, chat_type: Union[str, list]) -> None:
+        self.chat_type = chat_type
 
-    async def __call__(self, message: types.Message) -> bool:
-        return message.chat.type in self.chat_types
+    async def __call__(self, message: Message) -> bool:
+        if isinstance(self.chat_type, str):
+            return message.chat.type == self.chat_type
+        else:
+            return message.chat.type in self.chat_type
+
+
+
 
 
 class IsAdmin(Filter):
